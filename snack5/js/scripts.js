0f1f4520f4745b0d7f09d847beb68bce13759560
giordanoale <a.giordano89@gmail.com>
2021-03-03 12:03:05 +0100

    Risolto snack6

--- snack5/js/scripts.js
+++ snack5/js/scripts.js
@@ -11,7 +11,7 @@ for (var i = 0; i < 6; i++) {
 
 // se è dispari inseriscilo nell’array.
 
-  if (numero % 2 == 1) {
+  if (!isNaN(numero) && numero % 2 == 1) {
     insieme.push(numero);
   }
 
---
+++ snack6/js/scripts.js
@@ -0,0 +1,10 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero
+// indicato dall’utente.
+
+do {
+  var numero = parseInt(prompt("Inserisci un numero"));
+} while (isNaN(numero))
+
+for (var i = 1; i < numero; numero--) {
+  console.log( Math.pow(numero, 3) );
+}
